d2152bf0efd5d0723cf91dc389768f50b1bc348f
maheshpishe <maheshpishe393@gmail.com>
2021-07-16 18:26:07 +0530

    allow hosts

--- crud/settings.py
+++ crud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'j#*u4!x2gq7aky_jtt7k%h(p%%m((db$g28-k^bv0d!cp44yaf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0:5000']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
